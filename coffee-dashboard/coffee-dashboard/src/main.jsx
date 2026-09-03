6c3d8917e99d79c696c0a6b48bd76cf2f47b92f2
GaufreGentille <raphimignon@gmail.com>
2026-09-03 09:00:20 +0200

    Update main.jsx

--- coffee-dashboard/coffee-dashboard/src/main.jsx
+++ coffee-dashboard/coffee-dashboard/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import StudioCam from './pages/StudioCam.jsx'
+import StudioView from './pages/StudioView.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {window.location.hash === '#studio-cam' ? <StudioCam /> : <App />}
+    {window.location.hash.startsWith('#studio-cam') ? <StudioCam />
+      : window.location.hash.startsWith('#studio-view') ? <StudioView />
+      : <App />}
   </React.StrictMode>,
 )
