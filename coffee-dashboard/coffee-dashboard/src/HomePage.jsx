5f8457e5e737a12c9a983e5b03929194e7c1bda7
GaufreGentille <raphimignon@gmail.com>
2026-09-22 11:57:09 +0200

    Change text color for Coffea arabica

--- coffee-dashboard/coffee-dashboard/src/HomePage.jsx
+++ coffee-dashboard/coffee-dashboard/src/HomePage.jsx
@@ -110,7 +110,7 @@ function Reveal() {
           />
           <figcaption>
             <span>PLANCHE 01</span>
-            <em style={{ color: '#da5d16', marginLeft: '.5em' }}>Coffea arabica</em>
+            <em style={{ color: '#0f1a2e', marginLeft: '.5em' }}>Coffea arabica</em>
             <small>cerise · parche · grain vert</small>
           </figcaption>
         </figure>
